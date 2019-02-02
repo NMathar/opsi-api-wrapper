e451125b3b1872d609489c2fefb340e57f689a4c
nico <nico.mathar@gmail.com>
2019-02-02 21:24:43 +0100

    change product tests

--- test/product.test.js
+++ test/product.test.js
@@ -4,32 +4,33 @@ let API = require('../src/api')
 let api = new API('https://localhost:4447', 'opsi', 'opsi')
 
 describe('Test OPSI API Product Actions', function () {
-	describe('#getProductActionList()', function () {
-		it('get product action list', function (done) {
-			api.serverIDs(function (servers) {
-				api.actionsForProduct('', servers[0], function (data) {
-					if(data.success){
-						assert.ok(data.success)
-					}else{
-						console.error('Error: ',data.message)
-						assert.ok(data.message)
-					}
-					done()
-				})
-			})
-		})
-	})
+	this.timeout(500)
+
+	//TODO: create product
+
+	// describe('#getProductActionList()', function () {
+	// 	it('get product action list', function (done) {
+	// 		api.serverIDs(function (servers) {
+	// 			api.actionsForProduct('', servers[0], function (data) {
+	// 				if(data.success){
+	// 					assert.ok(data.success)
+	// 				}else{
+	// 					console.error('Error: ',data.message)
+	// 					assert.ok(data.message)
+	// 				}
+	// 				done()
+	// 			})
+	// 		})
+	// 	})
+	// })
 
 	describe('#getAllProducts()', function () {
 		it('get all products', function (done) {
-			api.getAllProducts(function (data) {
+			api.getAllProducts(function (res) {
 				// console.log('products ', data)
-				if(data.success){
-					assert.ok(data.success)
-				}else{
-					console.error('Error: ',data.message)
-					assert.ok(data.message)
-				}
+				assert.ok(res.success)
+				assert.ok(res.data instanceof Array, 'Array Expected')
+				console.log(res)
 				done()
 			})
 		})
