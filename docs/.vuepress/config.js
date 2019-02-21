65b5e314d33e9dfb982403606cef67c3d5b4777e
nico <nico.mathar@gmail.com>
2019-02-21 18:23:14 +0100

    add vuepress config and add npm to travis

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  base: '/opsi-api-wrapper/',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Repo', link: 'https://github.com/NMathar/opsi-api-wrapper' },
+    ]
+  }
+};
