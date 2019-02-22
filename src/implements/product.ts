99f3c61e74e3ea154dc9fac7e5e0fe6fa884e692
nico <nico.mathar@gmail.com>
2019-02-22 19:46:11 +0100

    add product documentation examples

--- src/implements/product.ts
+++ src/implements/product.ts
@@ -3,11 +3,15 @@ import { IfcResult } from '../interfaces/IfcResult';
 
 class Product {
   /**
-   * Get all products.
+   * Get all installed products from opsi depot.
    *
    * @example
-   * //returns array of all clients
-   * api.getAllProducts()
+   * ```typescript
+   * const { success, data, message } = await api.getAllProducts()
+   * console.log(success) // if all data are ok then this should return true else false
+   * console.log(message) // message is empty if success is true. if success is false there is a error message
+   * console.log(data) // array of opsi products
+   * ```
    *
    * @returns {IfcResult} Data.
    */
@@ -17,11 +21,15 @@ class Product {
   }
 
   /**
-   * get Product infos
+   * get detailed Product infos
    *
    * @example
-   * // returns an object of product data
-   * api.getProductInfo('swaudit')
+   * ```typescript
+   * const { success, data, message } = await api.getProductInfo('swaudit')
+   * console.log(success) // if all data are ok then this should return true else false
+   * console.log(message) // message is empty if success is true. if success is false there is a error message
+   * console.log(data) // object of detailed product information
+   * ```
    *
    * @param productId
    */
@@ -46,7 +54,14 @@ class Product {
    * Get all actions for one product.
    *
    * @example
-   * //returns array of actions for product
+   * @example
+   * ```typescript
+   * const server = await api.getServerIDs();
+   * const { success, data, message } = await api.getAllActionsForProduct('swaudit', server.data[0]);
+   * console.log(success) // if all data are ok then this should return true else false
+   * console.log(message) // message is empty if success is true. if success is false there is a error message
+   * console.log(data) // array of product actions
+   * ```
    * api.serverIDs()
    * api.actionsForProduct('', servers[0])
    *
